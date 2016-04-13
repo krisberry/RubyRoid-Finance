d22c0d168de403735b74ea06da5915e69f0ae126
krisberry <tina.doskich@gmail.com>
2016-04-13 19:06:21 +0300

    add small images to users table

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 
 class Image < ActiveRecord::Base
   belongs_to :imageable, :polymorphic => true
-  has_attached_file :photo, styles: { thumb: ["250x"]}
+  has_attached_file :photo, styles: { thumb: ["256x"], mini: ["50x50#"]}
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
