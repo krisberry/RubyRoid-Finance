6288fb1d478874b8a780c4755e9e11f81aeb5dff
krisberry <tina.doskich@gmail.com>
2016-04-27 19:01:45 +0300

    started rspec for invitations

--- spec/controllers/admin/events_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Admin::EventsController, type: :controller do
-
-end
--- spec/controllers/admin/invitations_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Admin::InvitationsController, type: :controller do
-
-end
